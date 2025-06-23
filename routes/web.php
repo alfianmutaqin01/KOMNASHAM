8392516d0c11c244875204a6af28e50bf7ec96af
Isa Maulana Tantra <isamaulanatantra@gmail.com>
2025-06-23 10:37:55 +0700

    Expose documentation template via route for easy access
    This commit adds a new route ('/template') that serves the 'documentation.html' file from the public directory. This allows developers to easily access and view the documentation template directly in the browser during development.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('template', function () {
+    return File::get(public_path() . '/documentation.html');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
