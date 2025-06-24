e8c024a598fa7bbe2d2982aa2196bd6ecb136118
Isa Maulana Tantra <isamaulanatantra@gmail.com>
2025-06-24 07:47:24 +0700

    Refactor code structure for improved readability and maintainability

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 Route::get('template', function () {
     return File::get(public_path() . '/documentation.html');
