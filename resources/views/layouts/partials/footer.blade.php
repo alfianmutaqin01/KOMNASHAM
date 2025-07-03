9f1a2d9c0ab1e1996e21436e9a0ebd11d0a989b8
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-07-03 11:25:17 +0700

    Remove notification offcanvas from footer and navbar components

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -15,59 +15,6 @@
     </div>
 </div>
 
-<div class="offcanvas offcanvas-end" tabindex="-1" id="notifications">
-    <div class="offcanvas-header py-0">
-        <h5 class="offcanvas-title py-3">Aktivitas Notifikasi</h5>
-        <button type="button" class="btn btn-light btn-sm btn-icon border-transparent rounded-pill"
-            data-bs-dismiss="offcanvas">
-            <i class="ph-x"></i>
-        </button>
-    </div>
-
-    <div class="offcanvas-body p-0">
-        <div class="bg-light fw-medium py-2 px-3">Notifikasi</div>
-        <div class="p-3">
-            <div class="d-flex align-items-start mb-3">
-                <a href="#" class="status-indicator-container me-3">
-                    <img src="{{ asset('limitless4.0/assets/images/demo/users/face1.jpg') }}"
-                        class="w-40px h-40px rounded-pill" alt="">
-                    <span class="status-indicator bg-success"></span>
-                </a>
-                <div class="flex-fill">
-                    <a href="#" class="fw-semibold">{{ Auth::user()->name }}</a> telah menyelesaikan verifikasi
-                    laporan <a href="#">#12345</a> dari daftar <a href="#">Pengaduan Baru</a>.
-                    <div class="fs-sm text-muted mt-1">2 jam yang lalu</div>
-                </div>
-            </div>
-
-            <div class="d-flex align-items-start mb-3">
-                <a href="#" class="status-indicator-container me-3">
-                    <img src="{{ asset('limitless4.0/assets/images/demo/users/face3.jpg') }}"
-                        class="w-40px h-40px rounded-pill" alt="">
-                    <span class="status-indicator bg-warning"></span>
-                </a>
-                <div class="flex-fill">
-                    <a href="#" class="fw-semibold">Sistem</a> menambahkan 4 laporan baru ke channel <span
-                        class="fw-semibold">Laporan Masyarakat</span>
-                    <div class="fs-sm text-muted mt-1">3 jam yang lalu</div>
-                </div>
-            </div>
-
-            <div class="d-flex align-items-start">
-                <div class="me-3">
-                    <div class="bg-warning bg-opacity-10 text-warning rounded-pill">
-                        <i class="ph-warning p-2"></i>
-                    </div>
-                </div>
-                <div class="flex-1">
-                    Peringatan: Kasus <a href="#">#98765</a> belum ada update selama 7 hari.
-                    <div class="fs-sm text-muted mt-1">4 jam yang lalu</div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 <div class="offcanvas offcanvas-end" tabindex="-1" id="demo_config">
     <div class="position-absolute top-50 end-100 visible">
         <button type="button" class="btn btn-outline-primary btn-icon translate-middle-y rounded-end-0"
