cea4bce5b8e24b935bf46b2acbc4d0f976eae627
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-07-08 15:38:45 +0700

    -

--- storage/framework/views/80fe19a956994d69624d7d74e4f2df9c.php
+++ storage/framework/views/80fe19a956994d69624d7d74e4f2df9c.php
@@ -3,11 +3,7 @@
         <div class="card-header">
             <h5 class="mb-0">Selamat Datang, <?php echo e(Auth::user()->name); ?>!</h5>
         </div>
-        <div class="card-body">
-            <p>Ini adalah halaman dashboard utama Anda. Berikut ringkasan aktivitas Komisi HAM Kabupaten Wonosobo:</p>
-        </div>
     </div>
-
     <?php
 $__split = function ($name, $params = []) {
     return [$name, $params];
